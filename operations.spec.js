c1da1f2c75bfa85dfc45c350a0dcf3f27db17c51
WENDYZYW\yiwen <cocozyw2811@gmail.com>
2018-11-24 13:42:51 +1100

    add controller test module

--- operations.spec.js
+++ operations.spec.js
@@ -1,7 +1,18 @@
 // Be descriptive with titles here. The describe and it titles combined read like a sentence.
-describe('first test', function() {
-    it('has a dummy spec to test 2 + 2', function() {
-        // An intentionally failing test. No code within expect() will never equal 4.
-        expect(2+2).toEqual(4);
+describe('CalculatorController', function() {
+    var $rootScope, $scope, $controller, calculatorController;
+
+    beforeEach(module('CalculatorApp'));
+
+    beforeEach(inject(function(_$rootScope_, _$controller_){
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        $controller = _$controller_;
+
+        calculatorController = $controller('CalculatorController', {'$rootScope' : $rootScope, '$scope': $scope});
+    }));
+
+    it('controller exist', function() {
+        expect(calculatorController).toBeDefined();
     });
 });
